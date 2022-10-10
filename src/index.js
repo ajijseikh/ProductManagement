ceba271159d1df026cc452ca67f37c0561bb4830
ajijsk <ajijsk240@gmail.com>
2022-10-10 16:13:12 +0530

    start

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-i 
+const express = require('express');
+const mongoose = require('mongoose');
+const router = require('./routes/route')
+const multer= require("multer");
+
+const app  = express();
+
+
+
+app.use(express.json())
+
+app.use( multer().any())
+
+const url="";
+mongoose.connect(url,{useNewUrlParser: true})
+.then(()=>console.log("MongoDb is Connected"))
+.catch(err=>console.log(err))
+
+app.use('/',router);
+
+app.listen(process.env.PORT || 3000, function (){
+    console.log('Server Started: '+3000)
+})
