12be8954edcc792294cbfd41ed4fbef28723e763
ajijsk <ajijsk240@gmail.com>
2022-10-12 14:14:08 +0530

    login

--- src/index.js
+++ src/index.js
@@ -11,13 +11,8 @@ app.use(express.json())
 
 app.use( multer().any())
 
-<<<<<<< HEAD
-const url="mongodb+srv://ajij:7pt2AejwcFh1o56K@cluster0.dwd5pcx.mongodb.net/test";
-mongoose.connect(url,{useNewUrlParser: true})
-=======
 // const url="";
 mongoose.connect("mongodb+srv://plutonium-batch:CD0Y7Vi1xxgIRocV@cluster0.78bw9.mongodb.net/test",{useNewUrlParser: true})
->>>>>>> d3a297eb9b193ff05cec0ef803bbb9cb2793d63e
 .then(()=>console.log("MongoDb is Connected"))
 .catch(err=>console.log(err))
 
--- src/routes/router.js
+++ src/routes/router.js
@@ -2,12 +2,9 @@ const express = require('express');
 const router = express.Router();
 const {createUser,UpdateUser}=require("../controllers/userController")
 
-<<<<<<< HEAD
 router.post("/register",createUser)
 router.put("/user/:userId/profile",UpdateUser)
 
-=======
 router.post("/login", login);
->>>>>>> d3a297eb9b193ff05cec0ef803bbb9cb2793d63e
 
 module.exports=router;
