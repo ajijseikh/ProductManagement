aefbd318986931bf9e78aab93dc6f9e5586ad25d
Priyanka <priyankashreevastava6@gmail.com>
2022-10-17 12:49:26 +0530

    cartModel

---
+++ src/models/cartModel.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const ObjectId = mongoose.Schema.Types.ObjectId;
+const cartSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: ObjectId,
+      ref: "userCollection",
+      required: true,
+      unique: true,
+    },
+
+    items: [
+      {
+        productId: {
+          type: ObjectId,
+          ref: "ProductCollection",
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+          min: 1,
+        },
+      },
+    ],
+    totalPrice: {
+      type: Number,
+      required: true,
+      //Holds total price of all the items in the cart
+    },
+    totalItems: {
+      type: Number,
+      required: true
+      //Holds total number of items in the cart
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("cart", cartSchema);
